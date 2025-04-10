a88a58a9187ad3b1f0b6bb9968c3690cda9c6812
deepa.a.ar <deepa.attiguppe@burendo.com>
2025-04-10 20:02:07 +0100

    updated browsers

--- src/test/java/com/test/web/utils/BaseTest.java
+++ src/test/java/com/test/web/utils/BaseTest.java
@@ -2,10 +2,6 @@ package com.test.web.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
